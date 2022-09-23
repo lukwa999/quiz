dcf69f56e00764ac41743ccb9447254c50731d13
lukwa999 <lukwa555@gmail.com>
2022-09-23 11:40:26 +0700

    demo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {createContext,useState} from "react";
+import Menu from "./component/Menu"
+import Quiz from "./component/Quiz";
+import Score from "./component/Score";
+
+import './App.css'
+
+export const DataContext = createContext()
+
+function App() {
+  const [appState, setAppState] = useState("menu")
+  const [score,setScore] = useState(0)
+  return (
+  <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+    <div className="App">
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu/>} {/* พิมพ์ตรงนี้จะ import ให้อัตโนมัตื */}
+        {appState === "quiz" && <Quiz/>} {/* พิมพ์ตรงนี้จะ import ให้อัตโนมัตื */}
+        {appState === "score" && <Score/>} {/* พิมพ์ตรงนี้จะ import ให้อัตโนมัตื */}
+    </div>
+  </DataContext.Provider>
+  );
+}
+
+export default App;
